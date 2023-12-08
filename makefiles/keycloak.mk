0ebaa8631f141690e8a7130cd1a3b1252c4df231
Nathan Monfils <nmo@escaux.com>
2023-12-08 15:12:20 +0100

    Improvement: Allow running tests in DIND
    Can't `curl localhost` from the CI as the CI localhost isn't DIND's
    localhost -> `docker exec ... curl localhost`
    
    Can't call other containers without doing --net host (or doing a complex
    thing with networks or switching to compose, none of which I want to do)

--- makefiles/keycloak.mk
+++ makefiles/keycloak.mk
@@ -1,6 +1,6 @@
 KEYCLOAK_IMAGE:=quay.io/keycloak/keycloak:15.0.2
 # KEYCLOAK_IMAGE:=quay.io/keycloak/keycloak:18.0.2 #--> Look deeper. There is a problem wir keycloak key rotation test results
-KEYCLOAK_CONTAINER_NAME:=kong_test_keycloack
+KEYCLOAK_CONTAINER_NAME:=kong_test_keycloak
 KEYCLOAK_PORT:=8080
 KEYCLOAK_ADMIN_USER:=admin
 KEYCLOAK_ADMIN_PASS:=admin
@@ -9,14 +9,16 @@ keycloak-start:
 	@echo "Running Keycloak..."
 	-- @docker start ${KEYCLOAK_CONTAINER_NAME} || docker run -d \
 	--name ${KEYCLOAK_CONTAINER_NAME} \
+	--net host \
 	-p ${KEYCLOAK_PORT}:8080 \
 	-e KEYCLOAK_USER=${KEYCLOAK_ADMIN_USER} \
 	-e KEYCLOAK_PASSWORD=${KEYCLOAK_ADMIN_PASS} \
 	-e KEYCLOAK_ADMIN=${KEYCLOAK_ADMIN_USER} \
 	-e KEYCLOAK_ADMIN_PASSWORD=${KEYCLOAK_ADMIN_PASS} \
-	${KEYCLOAK_IMAGE}
+	${KEYCLOAK_IMAGE} \
+	-b 0.0.0.0 -Djboss.https.port=8543 # needed to avoid conflict with nginx when using --net host
     # start-dev --http-relative-path /auth # needed starting from keycloak 18
-	@bash -c 'while ! timeout 1 bash -c "echo > /dev/tcp/localhost/8080"; do sleep 1; done'
+	@sh -c 'while ! docker exec ${KEYCLOAK_CONTAINER_NAME} curl -sf --max-time 1 localhost:8080; do echo "Waiting for keycloak..."; sleep 1; done'
 
 keycloak-stop:
 	@echo "Stopping Keycloak"
