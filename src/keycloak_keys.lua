94da81d4ad63abd6ec99ed5d44b28ca4a4d839bc
Nathan Monfils <nmo@escaux.com>
2024-05-07 17:08:31 +0200

    Hotfix: Use resty http library
    The https://github.com/lunarmodules/luasocket library seems
    ill-maintained and looks to be the cause of the following outage in
    production:
    
    2024/05/06 13:57:29 [debug] 1296#0: *462666 [kong] handler.lua:67 [jwt-keycloak] Getting public keys from token issuer
    2024/05/06 13:57:29 [debug] 1295#0: *462385 [kong] handler.lua:67 [jwt-keycloak] Getting public keys from token issuer
    2024/05/06 13:58:21 [notice] 1#0: signal 15 (SIGTERM) received from 1571, exiting
    
    So we're hoping to fix that by using the much more standard openresty
    http client.

--- src/keycloak_keys.lua
+++ src/keycloak_keys.lua
@@ -1,37 +1,30 @@
 local url = require "socket.url"
-local http = require "socket.http"
-local https = require "ssl.https"
+local http = require("resty.http")
 local cjson_safe = require "cjson.safe"
 local convert = require "kong.plugins.jwt-keycloak.key_conversion"
 
-local function get_request(url, scheme, port)
-    local req
-    if scheme == "https" then
-        req = https.request
-    else
-        req = http.request
+local function get_request(url)
+    -- Can't instanciate this in the module scope because the `require` can't be done at any time:
+    -- https://github.com/Kong/kong/issues/3271
+    local httpc = http.new()
+
+    local res, err = httpc:request_uri(url, {
+        method = "GET",
+    })
+
+    if err then
+        return nil, 'Error while calling url ' .. url .. ': ' .. err
     end
 
-    local res
-    local status
-    local err
-
-    local chunks = {}
-    res, status = req{
-        url = url,
-        port = port,
-        sink = ltn12.sink.table(chunks)
-    }
-    
-    if status ~= 200 then
-        return nil, 'Failed calling url ' .. url .. ' response status ' .. status
+    if res.status ~= 200 then
+        return nil, 'Failed calling url ' .. url .. ' response status ' .. res.status
     end
 
-    res, err = cjson_safe.decode(table.concat(chunks))
+    res, err = cjson_safe.decode(res.body)
     if not res then
         return nil, 'Failed to parse json response'
     end
-    
+
     return res, nil
 end
 
@@ -40,15 +33,12 @@ local function get_wellknown_endpoint(well_known_template, issuer)
 end
 
 local function get_issuer_keys(well_known_endpoint)
-    -- Get port of the request: This is done because keycloak 3.X.X does not play well with lua socket.http
-    local req = url.parse(well_known_endpoint)
-
-    local res, err = get_request(well_known_endpoint, req.scheme, req.port)
+    local res, err = get_request(well_known_endpoint)
     if err then
         return nil, err
     end
 
-    local res, err = get_request(res['jwks_uri'], req.scheme,  req.port)
+    local res, err = get_request(res['jwks_uri'])
     if err then
         return nil, err
     end
